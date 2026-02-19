5ede206b6411375fb62aad5a9eb0d8ceca14c37a
Matteo Cavallaro <teoc98@users.noreply.github.com>
2026-02-19 18:11:30 +0100

    add more private IP classes

--- src/owl/__init__.py
+++ src/owl/__init__.py
@@ -22,11 +22,26 @@ PROGRAM_NAME = "owl"
 PROGRAM_DESCRIPTION = "monitor Online Windows Laptops on the local network 🦉"
 DEFAULT_CACHE_FILENAME = "cache.sqlite"
 
-PRIVATE_IPV4_CLASSES = [
-    ipaddress.IPv4Network(("10.0.0.0", "255.0.0.0")),
-    ipaddress.IPv4Network(("172.16.0.0", "255.240.0.0")),
-    ipaddress.IPv4Network(("192.168.0.0", "255.255.0.0")),
+# https://it.wikipedia.org/wiki/Indirizzo_IP_privato#Classi_di_indirizzi
+PRIVATE_IPV4_CLASSES_RANGES = [
+    "10.0.0.0/8",
+    "172.16.0.0/12",
+    "192.168.0.0/16",
+    "0.0.0.0/8",
+    "100.64.0.0/10",
+    "127.0.0.0/8",
+    "169.254.0.0/16",
+    "192.0.0.0/24",
+    "192.0.2.0/24",
+    "192.88.99.0/24",
+    "198.18.0.0/15",
+    "198.51.100.0/24",
+    "203.0.113.0/24",
+    "224.0.0.0/4",
+    "240.0.0.0/4",
+    "255.255.255.255/32",
 ]
+PRIVATE_IPV4_CLASSES = [ipaddress.IPv4Network(r) for r in PRIVATE_IPV4_CLASSES_RANGES]
 
 
 class Base(DeclarativeBase):
